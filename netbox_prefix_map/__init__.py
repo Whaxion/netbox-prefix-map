024d3e0b0d89051a1874d058e11d91efdbf990df
Nicolas Martinussen <me+github@whaxion.be>
2024-09-17 14:34:38 +0200

    Bump version to 0.1.2

--- netbox_prefix_map/__init__.py
+++ netbox_prefix_map/__init__.py
@@ -4,7 +4,7 @@ class NetBoxPrefixMapConfig(PluginConfig):
     name = 'netbox_prefix_map'
     verbose_name = ' NetBox Prefix Map'
     description = 'View prefixes in a map in NetBox'
-    version = '0.1'
+    version = '0.1.2'
     base_url = 'prefix-map'
 
 config = NetBoxPrefixMapConfig
